98e03b7cfa111318201352f5411d3e33e321635f
FineGo-Team <egowidiarto@gmail.com>
2025-06-26 13:07:32 +0700

    feat(Dashboard): add total innovator by band level tabel

--- resources/views/components/dashboard/innovator/total-innovator-by-band-level.blade.php
+++ resources/views/components/dashboard/innovator/total-innovator-by-band-level.blade.php
@@ -1,3 +1,27 @@
-<div>
-    <!-- The whole future lies in uncertainty: live immediately. - Seneca -->
+<div class="card team-card border-0 shadow-lg mt-3">
+    <div class="card-header bg-gradient-primary">
+        <h5 class="card-title text-white">Total Inovator Berdasarkan Band Level</h5>
+    </div>
+    <div class="card-body">
+        <table class="table table-striped table-hover">
+            <thead>
+                <tr>
+                    <th>Band Level</th>
+                    @foreach ($years as $year)
+                        <th class="text-center">{{ $year }}</th>
+                    @endforeach
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($innovatorData as $band => $yearCount)
+                    <tr>
+                        <td>{{ $band }}</td>
+                        @foreach ($yearCount as $count)
+                            <td class="text-center">{{ $count }}</td>
+                        @endforeach
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 </div>
